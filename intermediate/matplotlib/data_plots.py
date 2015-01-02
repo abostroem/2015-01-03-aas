7db1a4ac655c4bc8c62f29d502f7c6b92b158359
Phil Rosenfield <philrosenfield@gmail.com>
2015-01-02 10:40:57 -0800

    finshed matplotlib lesson, probably no time for aplpy

--- intermediate/matplotlib/data_plots.py
+++ intermediate/matplotlib/data_plots.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
-
+""" 
+Script to make CMD, LF, or Hess diagram from a binary fits table
+Written by: Phil Rosenfield
+"""
 from astropy.io import fits
 import matplotlib.pylab as plt
 import numpy as np
 import argparse
 import sys
 
-plt.style.use('presentation')
-
-
-def cmd(color, mag, color_err=None, mag_err=None):
-    '''Color Magnitude diagram with uncertainties'''
+def cmd(color, mag, color_err=None, mag_err=None, ax=None):
+    '''
+    Plot a Color Magnitude diagram with uncertainties
+    
+    Parameters
+    ----------
+    color, mag : color and magnitude arrays
+    color_err, mag_err: uncertainties in color and mag
+    
+    ax : axes instance
+    
+    Returns
+    -------
+    ax : axes instance
+    '''
 
     # the fits file contains stars that are recovered in only one filter
     # stars not recovered are given values >= 90. No need to plot em.
@@ -19,67 +32,83 @@ def cmd(color, mag, color_err=None, mag_err=None):
     if ax is None:
         fig, ax = plt.subplots(figsize=(8, 8))
 
-    ax.plot(color[good], mag[good], '.', color='black', ms=3)
+    ax.plot(color[good], mag[good], '.', ms=3)
 
     if color_err is not None and mag_err is not None:
-        ax.errorbar(color[good], mag[good], fmt='none', xerr=color_err[good],
+        ax.errorbar(color[good], mag[good], fmt='none', lw=1, xerr=color_err[good],
                     yerr=mag_err[good], capsize=0, ecolor='gray')
 
+    # reverse yaxis
+    ax.set_ylim(ax.get_ylim()[::-1])
     return ax
 
 
 def plot_lf(mag, binsize, mbin=None, yscale='log'):
+    """
+    Make a Luminosty function (binned magnitude) plot
+    
+    Parameters
+    ----------
+    mag : array
+        magnitude array to be binned
+    binsize : float
+        width of magnitude bins
+    mbin : array
+        right edges of magnitude bins
+    yscale : str
+        plt.set_yscale option
+
+    Returns
+    -------
+    ax : axes instance
+    """
     if mbin is None:
         mbin = np.arange(mag.min(), mag.max(), binsize)
     
     lf, bins = np.histogram(mag, bins=mbin)
     fig, ax = plt.subplots()
     
-    ax.plot(bins[1:], lf, linestyle='steps-pre', lw=3, color='k')
+    ax.plot(bins[1:], lf, linestyle='steps-pre')
     ax.set_yscale(yscale)
     return ax
     
-def make_hess(color, mag, binsize, **kw):
+def make_hess(color, mag, binsize, cbinsize=None, mbin=None, cbin=None):
     """
     Compute a hess diagram (surface-density CMD) on photometry data.
 
-    Paramters
+    Parameters
     ---------
-    color: array
+    color : array
         color values
 
-    mag: array
+    mag : array
         magnitude values
 
     binsize, cbinsize: float, float
         width of mag, color bins in magnitudes
 
-    cbin: sequence, optional
-        set the centers of the color bins
+    cbin : array
+        the right edges of the color bins
 
-    mbin: sequence, optional
-        set the centers of the magnitude bins
+    mbin : array
+        the right edges of the magnitude bins
 
     Returns
     -------
-    cbin: sequence
+    cbin : array
         the centers of the color bins
 
-    mbin: sequence
+    mbin : array
         the centers of the magnitude bins
 
-    hess:
-        The Hess diagram array
+    hess : 2d array
+        The Hess diagram values
     """
-
-    mbin = kw.get('mbin')
-    cbin = kw.get('cbin')
 
     if mbin is None:
         mbin = np.arange(mag.min(), mag.max(), binsize)
 
     if cbin is None:
-        cbinsize = kw.get('cbinsize')
         if cbinsize is None:
             cbinsize = binsize
         cbin = np.arange(color.min(), color.max(), cbinsize)
@@ -90,7 +119,36 @@ def make_hess(color, mag, binsize, **kw):
 
 def plot_hess(color, mag, binsize, ax=None, colorbar=False,
               vmin=None, vmax=None, cbinsize=None, im_kwargs={}):
-    '''Plot a hess diagram with imshow.'''
+    """
+    Plot a hess diagram with imshow.
+    
+    Parameters
+    ----------
+    color : array
+        color array to be binned
+    mag : array
+        magnitude array to be binned
+
+    binsize, cbinsize : float, float
+        width of magnitude, color bins
+    
+    colorbar : bool
+        option to also plot the colorbar
+    
+    vmin, vmax : float, float or None, None
+        lower and upper limits sent to matplotlib.colors.LogNorm
+    
+    im_kwargs : dict
+        dictionary passed to imshow by default:
+        defaults = {'norm': LogNorm(vmin=vmin, vmax=vmax),
+                    'cmap': plt.cm.gray,
+                    'interpolation': 'nearest',
+                    'extent' [limits of mag and color]
+                    'aspect': 'auto'}
+    Returns
+    -------
+    ax : axes instance
+    """
     from matplotlib.colors import LogNorm
     if ax is None:
         fig, ax = plt.subplots()
@@ -103,11 +161,10 @@ def plot_hess(color, mag, binsize, ax=None, colorbar=False,
                 'cmap': plt.cm.gray,
                 'interpolation': 'nearest',
                 'extent': extent,
-                'aspect': 1.}
+                'aspect': 'auto'}
 
     kwargs = dict(defaults.items() + im_kwargs.items())
 
-    ax.autoscale(False)
     im = ax.imshow(hess.T, **kwargs)
 
     if colorbar is True:
@@ -117,6 +174,23 @@ def plot_hess(color, mag, binsize, ax=None, colorbar=False,
 
 
 def load_data(fitsfile, yfilt='I'):
+    """
+    Load color, magnitude and uncertainties from binary fits table
+    
+    Parameters
+    ----------
+    fitsfile : string or file object
+        path to binary fits table or object to be read by astropy.io.fits
+    
+    yfilt : string
+        filter to use as mag (V or I)
+
+    Returns
+    -------
+    color, mag : arrays of color and magnitude
+    color_error, mag_err : arrays of summed quadriture uncertainies and magnitude uncertainties
+    """
+
     hdu = fits.open(fitsfile)
     data = hdu[1].data
     photsys = hdu[0].header['CAMERA']
@@ -152,7 +226,7 @@ def main(argv):
                         help='comma separated V and I filter names for plot labels')
 
     parser.add_argument('-y', '--yfilter', type=str, default='I',
-                        help='plot V or I on y axis or on LF')
+                        help='plot V or I for LF or on y axis for cmd, Hess')
 
     parser.add_argument('-m', '--binsize', type=float, default=0.05,
                         help='hess diagram or LF mag binsize')
@@ -160,27 +234,38 @@ def main(argv):
     parser.add_argument('-c', '--cbinsize', type=float, default=0.1,
                         help='hess diagram color binsize')
 
-    parser.add_argument('-xlim', '--xlim', type=str, default=None,
-                        help='x axis limit')
+    parser.add_argument('-xlim', '--xlim', type=list, default=None,
+                        help='comma separated x axis min, max')
 
-    parser.add_argument('-ylim', '--ylim', type=str, default=None,
-                        help='y axis limit')
+    parser.add_argument('-ylim', '--ylim', type=list, default=None,
+                        help='comma separated y axis min, max')
 
     parser.add_argument('-yscale', '--yscale', type=str, default='log',
                         help='y axis scale for LF')
 
-    parser.add_argument('-x', '--colorbar', type=bool, default=False,
-                        help='cmd y axis limit')
+    parser.add_argument('-x', '--colorbar', action='store_true',
+                        help='add the hess diagram colorbar')
+
+    parser.add_argument('-outfile', '--outfile', type=str, default='data_plot.png',
+                        help='the name of the output file')
+
+    parser.add_argument('-style', '--style', type=str, default='ggplot',
+                        choices=plt.style.available,
+                        help='the name of the matplotlib style')
 
     parser.add_argument('file', type=argparse.FileType('r'),
                         help='the name of the fits file')
 
-    parser.add_argument('-outfile', '--outfile', type=str, default=None,
-                        help='the name of the output file')
-
     args = parser.parse_args(argv)
-    
-    filter1, filter2 = args.filters.split(',')
+
+    # set the plot style
+    plt.style.use(args.style)
+
+    if args.filters is not None:
+        filter1, filter2 = args.filters.split(',')
+    else:
+        filter1 = 'V'
+        filter2 = 'I'
     
     yfilt = args.yfilter
     
@@ -195,35 +280,31 @@ def main(argv):
 
     if args.plottype.lower() == 'lf':
         ax = plot_lf(mag, args.binsize, yscale=args.yscale)
+
+        # make axis labels
         ax.set_xlabel(r'$%s$' % yfilt)
         ax.set_ylabel(r'$\#$')
     else:
-        if filter2 is not None and filter1 is not None:
+        # make axis labels for cmd, hess
+        if args.filters is not None:
             if yfilt == 'I':
                 yfilt = filter2
             else:
                 yfilt = filter1
 
-            ax.set_ylabel(r'$%s$' % yfilt)
-            ax.set_xlabel(r'$%s-%s$' % (filter1, filter2))
+        ax.set_ylabel(r'$%s$' % yfilt)
+        ax.set_xlabel(r'$%s-%s$' % (filter1, filter2))
 
-        if args.ylim is None:
-            # reverse yaxis
-            args.ylim = ax.get_ylim()[::-1]
-        ax.set_ylim(args.ylim)
+    if args.ylim is not None:
+        ylim = np.array(''.join(args.ylim).split(','), dtype=float)
+        ax.set_ylim(ylim)
     
     if args.xlim is not None:
-        ax.set_xlim(args.xlim)
-
-    if args.outfile is None:
-        args.outfile = 'data_plot.png'
+        xlim = np.array(''.join(args.xlim).split(','), dtype=float)
+        ax.set_xlim(xlim)
 
     plt.savefig(args.outfile)
     print('wrote %s' % args.outfile)
 
-# Click on CMD
-
 if __name__ == "__main__":
     main(sys.argv[1:])
-    # http://archive.stsci.edu/prepds/angst/datalist.html
-    # http://archive.stsci.edu/pub/hlsp/angst/acs/hlsp_angst_hst_acs-wfc_10605-ugc-5336_f555w-f814w_v1_gst.fits
