fa09fb0f8ed8dc1534c04ea1033e9ddc043ee3d8
Matt Davis <jiffyclub@gmail.com>
2015-01-02 15:31:43 -0800

    uglify the code-review sample a bit more

--- novice/code-review/data_processor.py
+++ novice/code-review/data_processor.py
@@ -13,27 +13,6 @@ def read_csv_file(input_csv_file):
     years, temperature, rainfall, mosquitos = d
     return d
 
-def process(dataset):
-    '''
-    process the data file and output the necessary analytics
-
-    we want standard deviation and variance for each column
-    '''
-    data = []
-    for arr_arr in dataset:
-      std_arr_arr = np.sqrt(np.mean(abs(arr_arr - np.mean(arr_arr)) * 2));
-      var_arr_arr = (np.mean(abs(arr_arr - np.mean(arr_arr)) ** 2));
-      max_arr_arr = max(arr_arr);
-      min_arr_arr = min(arr_arr);
-      print max_arr_arr, min_arr_arr;
-      data.append([std_arr_arr, var_arr_arr]);
-    return data
-
-def write_file(ofile, data):
-    ofile = open(ofile, 'w')
-    for row in data:
-        ofile.write(np.array_str(np.array(row)))
-    ofile.close()
 
 # sys.argv contains all the parameters passed
 # to the program. assume the 1st is a filename
@@ -44,5 +23,18 @@ if not input_file:
     print "need a file to process!"
     exit(1)
 p = read_csv_file(input_file)
-pr = process(p)
-write_file(ofile, pr)
+
+data = []
+for arr_arr in p:
+  std_arr_arr = np.sqrt(np.mean(abs(arr_arr - np.mean(arr_arr)) * 2));
+  var_arr_arr = (np.mean(abs(arr_arr - np.mean(arr_arr)) ** 2));
+  max_arr_arr = max(arr_arr);
+  min_arr_arr = min(arr_arr);
+  print max_arr_arr, min_arr_arr;
+  data.append([std_arr_arr, var_arr_arr]);
+
+
+ofile = open(ofile, 'w')
+for row in data:
+    ofile.write(np.array_str(np.array(row)))
+ofile.close()
